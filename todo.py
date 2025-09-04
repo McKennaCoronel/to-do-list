17cbfb558dde46f9b04eff532a5d8feff3a970f9
McKennaCoronel <mckennacoronel1@gmail.com>
2025-09-04 10:41:09 -0500

    mark complete works

--- todo.py
+++ todo.py
@@ -19,6 +19,10 @@ def delete_tasks(index):
 
 
 # Step 5: Mark task complete
+def mark_complete(index):
+    if 0 <= index < len(tasks):
+        tasks[index] = tasks[index] + "😛"
+
 
 
 # Step 6: Save/load tasks (extra stretch for today)
@@ -31,6 +35,6 @@ if __name__ == "__main__":
     view_tasks()
     delete_tasks(0)
     view_tasks()
-    #mark_complete(0)
-    #view_tasks()
+    mark_complete(0)
+    view_tasks()
     #save_tasks()
